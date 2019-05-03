5a3547da0adb7d3a28c95686f3ce9197bb56ea90
Samuel Rendon G <s.redon.g@gmail.com>
2019-05-03 17:31:21 -0500

    products5

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::ProductsController < ApplicationController::Api
   def index
       @products = Product.all
-      render json: @products
+      # render json: @products
+
+      respond_to do |format|
+
+            format.json { render :index, status: :ok, location: @products }
+
+      end
   end
+
+
 end
